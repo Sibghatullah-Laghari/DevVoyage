0bc8b53b74dfca7c833f25fef16679c0c3860542
Sibghatullah Laghari <Siblaghari@gmail.com>
2026-08-04 13:53:44 +0500

    Update BackendApplication.java

--- backend/src/main/java/com/devvoyage/backend/BackendApplication.java
+++ backend/src/main/java/com/devvoyage/backend/BackendApplication.java
@@ -3,6 +3,8 @@ package com.devvoyage.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// main file
+
 @SpringBootApplication
 public class BackendApplication {
 
